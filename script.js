eebdb47bfeb3d05951aaba8f0fed0eb1d514ec1f
GulistaKhatun06 <160149107+GulistaKhatun06@users.noreply.github.com>
2024-06-17 23:19:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let string="";
 
-//jinka button class hao wo select ho jayenge
+
 let buttons=  document.querySelectorAll('.button');
-//yek array bano buttons ke liye
+
 Array.from(buttons).forEach((button)=>{
-//jinpe click liya jaye to wo print ho
+
 button.addEventListener('click', (e) =>{
     if(e.target.innerHTML=='='){
         string = eval(string);
@@ -17,7 +17,7 @@ button.addEventListener('click', (e) =>{
 else{
 console.log(e.target);
 string = string+e.target.innerHTML;
-// input ko tag karna hai aur usko string me convert kar do
+
 document.querySelector('input').value = string;
 }
 })
